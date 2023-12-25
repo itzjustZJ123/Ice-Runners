599564330efddfd64af8767d70bd88cfe91a200f
Ice Runners Inc <91702939+itzjustZJ123@users.noreply.github.com>
2023-12-25 12:49:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 // Function to open tabs
-function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+function openTab(tabName) {
+  const tabcontent = document.querySelectorAll(".tabcontent");
+  const tablinks = document.querySelectorAll(".tablinks");
+
+  tabcontent.forEach(tab => {
+    tab.style.display = "none";
+  });
+
+  tablinks.forEach(link => {
+    link.classList.remove("active");
+  });
+
   document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+  document.querySelector(`[data-tab="${tabName}"]`).classList.add("active");
 }
 
 // Function to create snowflakes
@@ -30,20 +32,64 @@ function createSnowflake() {
   }, 5000);
 }
 
-// Create a new snowflake every 300ms
+// Call the function to create a new snowflake every 300ms
 setInterval(createSnowflake, 300);
 
 // Function to display the Description tab initially
 function showDescriptionTab() {
-  openTab(event, 'Description');
+  openTab('Description');
 }
 
 // Call the function to show the Description tab when the page loads
 window.addEventListener('load', showDescriptionTab);
-// Function to display the Description tab initially
-function showDescriptionTab() {
-  openTab(event, 'Description');
+// Function to open tabs
+function openTab(tabName) {
+  const tabcontent = document.querySelectorAll(".tabcontent");
+  const tablinks = document.querySelectorAll(".tablinks");
+
+  tabcontent.forEach(tab => {
+    tab.style.display = "none";
+  });
+
+  tablinks.forEach(link => {
+    link.classList.remove("active");
+  });
+
+  document.getElementById(tabName).style.display = "block";
+  document.querySelector(`[data-tab="${tabName}"]`).classList.add("active");
 }
 
-// Attach the showDescriptionTab function to the DOMContentLoaded event
-document.addEventListener('DOMContentLoaded', showDescriptionTab);
+// Function to create snowflakes
+function createSnowflake() {
+  const snowflake = document.createElement('div');
+  snowflake.classList.add('snowflake');
+  snowflake.innerHTML = '❄️';
+  snowflake.style.left = Math.random() * 100 + 'vw';
+  snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
+  snowflake.style.opacity = Math.random();
+  snowflake.style.fontSize = Math.random() * 20 + 10 + 'px';
+
+  document.body.appendChild(snowflake);
+
+  setTimeout(() => {
+    snowflake.remove();
+  }, 5000);
+}
+
+// Call the function to create a new snowflake every 300ms
+setInterval(createSnowflake, 300);
+
+// Function to display the Description tab initially
+function showDescriptionTab() {
+  openTab('Description');
+}
+
+// Call the function to show the Description tab when the page loads
+window.addEventListener('load', showDescriptionTab);
+const tabButtons = document.querySelectorAll(".tablinks");
+tabButtons.forEach(button => {
+  button.addEventListener("click", function() {
+    const tabName = this.getAttribute("data-tab");
+    openTab(tabName);
+  });
+});
